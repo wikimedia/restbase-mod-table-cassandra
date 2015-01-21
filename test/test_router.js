e50536a903f99c020c9a7b78719d7aee74fec8e2
Marko Obrovac <marko.obrovac@gmail.com>
2015-01-20 16:50:04 -0800

    Test router: turn module exported object into a routeswitch handler
    Instead of exporting handlers directly, the module now exports a spec
    and an operation object. However, in the tests we use routeswitch which
    expects concrete handler methods. This commit translates the module's
    exported object into a handler object, checking all of the needed
    operationId's have been exported by the module in the process.

--- test/test_router.js
+++ test/test_router.js
@@ -24,35 +24,55 @@ function setupConfigDefaults(conf) {
 
 var router = {};
 router.request = function(req) {
-	var match = this.newRouter.match(req.url);
-	if (match) {
-		req.params = match.params;
-		var handler = match.methods[req.method.toLowerCase()];
-		if (handler && handler.request_handler) {
-			return handler.request_handler({}, req)
-			.then(function(item){
-				return item;
-			});
-		}
-	}
+    var match = this.newRouter.match(req.url);
+    if (match) {
+        req.params = match.params;
+        var handler = match.methods[req.method.toLowerCase()];
+        if (handler) {
+            return handler({}, req)
+            .then(function(item){
+                return item;
+            });
+        } else {
+            throw new Error('No handler for ' + req.method + ' ' + req.url);
+        }
+    } else {
+        throw new Error('No match for ' + req.method + ' ' + req.url);
+    }
 };
 
-router.makeRouter = function(req) {
-	var self = this;
-	var conf = setupConfigDefaults();
-	var opt = {
-		log: function(){},
-		conf: conf
-	};
+function flatHandlerFromModDef (modDef, prefix) {
+    var handler = { paths: {} };
+    Object.keys(modDef.spec.paths).forEach(function(path) {
+        var pathModSpec = modDef.spec.paths[path];
+        handler.paths[prefix + path] = {};
+        Object.keys(pathModSpec).forEach(function(m) {
+            var opId = pathModSpec[m].operationId;
+            if (!modDef.operations[opId]) {
+                throw new Error('The module does not export the opration ' + opId);
+            }
+            handler.paths[prefix + path][m] = modDef.operations[opId];
+        });
+    });
+    return handler;
+}
 
-	return require('../index.js')(opt)
-	.then(function(handler) {
-		self.newRouter = new RouteSwitch.fromHandlers([handler]);
-		return self;
-	})
-	.catch(function(e) {
-		console.log('Error during RESTBase startup: ', e);
-	});
+router.makeRouter = function(req) {
+    var self = this;
+    var conf = setupConfigDefaults();
+    var opt = {
+        log: function(){},
+        conf: conf
+    };
+
+    return require('../index.js')(opt)
+    .then(function(modDef) {
+        self.newRouter = new RouteSwitch.fromHandlers([flatHandlerFromModDef(modDef, '/{domain}/sys')]);
+        return self;
+    })
+    .catch(function(e) {
+        console.log('Error during RESTBase startup: ', e);
+    });
 };
 
 module.exports = router;
