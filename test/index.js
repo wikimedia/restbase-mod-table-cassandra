8618626953a053e4ec0b4fb42cdd362f501480fc
Petr Pchelko <ppchelko@wikimedia.org>
2015-07-09 11:30:21 +0300

    Moved tests to a separate module
    The tests and the spec are moved to a separate module:
    https://github.com/wikimedia/restbase-mod-table-spec

--- test/index.js
+++ test/index.js
@@ -1,8 +1,21 @@
 "use strict";
 
-
 // mocha defines to avoid JSHint breakage
 /* global describe, it, context, before, beforeEach, after, afterEach */
 
+var yaml = require('js-yaml');
+var fs = require("fs");
+
 // Run jshint as part of normal testing
 require('mocha-jshint')();
+
+describe('Functional', function() {
+    var conf = yaml.safeLoad(fs.readFileSync(__dirname + '/utils/test_client.conf.yaml'));
+    var dbConstructor = require('../index.js');
+    require('restbase-mod-table-spec').test(function() {
+        return dbConstructor({
+            conf: conf,
+            log: function() {}
+        });
+    });
+});
