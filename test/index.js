5d39eac4f320ad8cfa6572a10b6b86fd9986ee22
Petr Pchelko <ppchelko@wikimedia.org>
2018-07-05 15:47:51 +0300

    Dependencies update
    1. Update all dependencies, mainly cassandra-driver to 3.5.0
    2. Adapt to newer eslint settings
    3. Remove jscs and jshint, they both are replaced with eslint

--- test/index.js
+++ test/index.js
@@ -1,27 +1,20 @@
 "use strict";
 
-// mocha defines to avoid JSHint breakage
-/* global describe, it, context, before, beforeEach, after, afterEach */
+const yaml = require('js-yaml');
+const fs = require("fs");
 
-var yaml = require('js-yaml');
-var fs = require("fs");
-
-// Run jshint as part of normal testing
-require('mocha-jshint')();
-require('mocha-jscs')();
 require('mocha-eslint')([
     'lib',
     'index.js'
 ]);
 
 
-describe('Functional', function() {
-    var conf = yaml.safeLoad(fs.readFileSync(__dirname + '/utils/test_client.conf.yaml'));
-    var dbConstructor = require('../index.js');
-    require('restbase-mod-table-spec').test(function() {
-        return dbConstructor({
-            conf: conf,
-            log: function() {}
-        });
-    });
+describe('Functional', () => {
+    const conf = yaml.safeLoad(fs.readFileSync(`${__dirname}/utils/test_client.conf.yaml`));
+    const dbConstructor = require('../index.js');
+    require('restbase-mod-table-spec').test(() => dbConstructor({
+        conf: conf,
+        log: function () {
+        }
+    }));
 });
